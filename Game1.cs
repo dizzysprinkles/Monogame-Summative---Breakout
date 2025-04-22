40945854821d74c79b6382c5735481100295d21f
Zoey Hamm <339622581@GoTVDSB.ca>
2025-04-22 14:34:43 -0400

    Added background textures and screen state

--- Game1.cs
+++ Game1.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Text.Encodings.Web;
 
 namespace Monogame_Summative___Breakout
 {
+    enum Screen
+    {
+        Title,
+        Tutorial,
+        Main,
+        End
+    }
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Rectangle window;
+        Screen screenState;
+        Texture2D titleBackgroundTexture, tutorialBackgroundTexture, mainBackgroundTexture, endBackgroundTexture;
+        KeyboardState currentKeyboardState, prevKeyboardState;
 
         public Game1()
         {
@@ -18,7 +32,11 @@ namespace Monogame_Summative___Breakout
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            screenState = Screen.Title;
+            window = new Rectangle(0,0,800,600);
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -27,7 +45,10 @@ namespace Monogame_Summative___Breakout
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            titleBackgroundTexture = Content.Load<Texture2D>("Images/titleBackground");
+            tutorialBackgroundTexture = Content.Load<Texture2D>("Images/tutorialBackground");
+            mainBackgroundTexture = Content.Load<Texture2D>("Images/mainBackground");
+            endBackgroundTexture = Content.Load<Texture2D>("Images/endBackground");
         }
 
         protected override void Update(GameTime gameTime)
@@ -42,10 +63,27 @@ namespace Monogame_Summative___Breakout
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
+            _spriteBatch.Begin();
 
-            // TODO: Add your drawing code here
+            if (screenState == Screen.Title)
+            {
+                _spriteBatch.Draw(titleBackgroundTexture, window, Color.White);
+            }
+            else if (screenState == Screen.Tutorial)
+            {
+                _spriteBatch.Draw(tutorialBackgroundTexture, window, Color.White);
+            }
+            else if (screenState == Screen.Main)
+            {
+                _spriteBatch.Draw(mainBackgroundTexture, window, Color.White);
+            }
+            else
+            {
+                _spriteBatch.Draw(endBackgroundTexture, window, Color.White);
+            }
 
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
